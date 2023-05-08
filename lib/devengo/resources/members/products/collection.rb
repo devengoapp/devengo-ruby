9e2fe038213102fec062a47f0333cfb00c672424
Nacho <nacho@devengo.com>
2023-05-08 15:35:20 +0200

    Update member name constructor

--- lib/devengo/resources/members/products/collection.rb
+++ lib/devengo/resources/members/products/collection.rb
@@ -8,6 +8,12 @@ module Devengo
           def self.from_raw(products)
             super items: products.map { |product| Product.from_raw(**product) }
           end
+
+          def self.from_raw_nullable(attributes)
+            return nil if attributes.nil?
+
+            from_raw(attributes)
+          end
         end
       end
     end
