bc9565d65cf23306fdfb5398f65ed7a3a1c531b3
Iván Guardado <dev.ivangc@gmail.com>
2023-05-09 19:21:19 +0200

    Update services_spec.rb

--- spec/devengo/api/services_spec.rb
+++ spec/devengo/api/services_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Devengo::API::Services, :unit, type: :api do
   let!(:client) { initialize_client }
 
   it "includes all expected services" do
-    expect(instance_methods_count(described_class)).to eq 7
+    expect(instance_methods_count(described_class)).to eq 8
     expect(client.accounts).to be_an_instance_of(Devengo::API::AccountsService)
     expect(client.incoming_payments).to be_an_instance_of(Devengo::API::IncomingPaymentsService)
     expect(client.auth).to be_an_instance_of(Devengo::API::AuthService)
@@ -12,5 +12,6 @@ RSpec.describe Devengo::API::Services, :unit, type: :api do
     expect(client.transactions).to be_an_instance_of(Devengo::API::TransactionsService)
     expect(client.webhooks).to be_an_instance_of(Devengo::API::WebhooksService)
     expect(client.webhook_requests).to be_an_instance_of(Devengo::API::WebhookRequestsService)
+    expect(client.verifications).to be_an_instance_of(Devengo::API::VerificationsService)
   end
 end
