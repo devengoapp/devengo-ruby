05a347afd90c9542b30b0d398d1c40d4877be065
Fran Moya <fran@devengo.com>
2023-05-10 09:16:14 +0200

    Include verifications service on services

--- spec/devengo/api/services_spec.rb
+++ spec/devengo/api/services_spec.rb
@@ -4,12 +4,13 @@ RSpec.describe Devengo::API::Services, :unit, type: :api do
   let!(:client) { initialize_client }
 
   it "includes all expected services" do
-    expect(instance_methods_count(described_class)).to eq 7
+    expect(instance_methods_count(described_class)).to eq 8
     expect(client.accounts).to be_an_instance_of(Devengo::API::AccountsService)
     expect(client.incoming_payments).to be_an_instance_of(Devengo::API::IncomingPaymentsService)
     expect(client.auth).to be_an_instance_of(Devengo::API::AuthService)
     expect(client.payments).to be_an_instance_of(Devengo::API::PaymentsService)
     expect(client.transactions).to be_an_instance_of(Devengo::API::TransactionsService)
+    expect(client.verifications).to be_an_instance_of(Devengo::API::VerificationsService)
     expect(client.webhooks).to be_an_instance_of(Devengo::API::WebhooksService)
     expect(client.webhook_requests).to be_an_instance_of(Devengo::API::WebhookRequestsService)
   end
