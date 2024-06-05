64c67f0e01fe40f2c59204ee57618ceb9590be72
Carlos <carlos@devengo.com>
2024-06-05 09:46:51 +0200

    Add new fields to error response

--- lib/devengo/errors/client.rb
+++ lib/devengo/errors/client.rb
@@ -3,13 +3,15 @@
 module Devengo
   module Errors
     class Client < Http
-      attr_reader :code, :type
+      attr_reader :code, :type, :title, :detail
 
       def initialize(api_response:, client_error:)
         super message: client_error[:message], api_response: api_response
 
         @code = client_error[:code]
         @type = client_error[:type]
+        @title = client_error[:title]
+        @detail = client_error[:detail]
       end
     end
   end
